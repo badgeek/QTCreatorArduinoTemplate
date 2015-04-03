69eb76d87827e00705a0cc98c08f67c7e26f93a2
Budi Prakosa a.k.a Iyok <iyok@deadmediafm.org>
2015-04-03 11:22:36 +0700

    Update blink example

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,53 +1,70 @@
-/*
-Arduino template for Qt
-Downloaded from http://www.lucidarme.me
-
-In the menu Tools>Option set in the Terminal field : xterm -e
-
-Set the following parameters in the project tab :
-Build directory : add /src at the end of the path
-Build steps : make all
-Clean steps : make clean
-
-
-*/
-
-
 // Access to the Arduino Libraries
 #include <Arduino.h>
 
-/*
-  Turns on an LED on for one second, then off for one second, repeatedly.
-*/
 
+/* Blink without Delay
 
-// Define the pin where the led is connected
-#define     LED_PIN     13
-#define     DELAY_MS    1000
+ Turns on and off a light emitting diode(LED) connected to a digital
+ pin, without using the delay() function.  This means that other code
+ can run at the same time without being interrupted by the LED code.
 
+ The circuit:
+ * LED attached from pin 13 to ground.
+ * Note: on most Arduinos, there is already an LED on the board
+ that's attached to pin 13, so no hardware is needed for this example.
+ 
+ created 2005
+ by David A. Mellis
+ modified 8 Feb 2010
+ by Paul Stoffregen
+ modified 11 Nov 2013
+ by Scott Fitzgerald
+ 
+ 
+ This example code is in the public domain.
+ 
+ http://www.arduino.cc/en/Tutorial/BlinkWithoutDelay
+ */
 
-int main()
-{
-    // Initialize Arduino Librairies
-    init();
+// constants won't change. Used here to set a pin number :
+const int ledPin =  13;      // the number of the LED pin
 
-    Serial.begin(115200);
-    Serial.println("http://www.lucidarme.me \nHello world !");
-    
-    // Set the led pin as output
-    pinMode(LED_PIN, OUTPUT);
-    
-    while (1)
-    {
-        // Set the LED on
-        digitalWrite(LED_PIN, HIGH);
-        // Wait for a second
-        delay(DELAY_MS);
-        // Set the LED off
-        digitalWrite(LED_PIN, LOW);
-        // Wait for a second
-        delay(DELAY_MS);
-    }
-    
+// Variables will change :
+int ledState = LOW;             // ledState used to set the LED
+
+// Generally, you shuould use "unsigned long" for variables that hold time
+// The value will quickly become too large for an int to store
+unsigned long previousMillis = 0;        // will store last time LED was updated
+
+// constants won't change :
+const long interval = 1000;           // interval at which to blink (milliseconds)
+
+void setup() {
+  // set the digital pin as output:
+  pinMode(ledPin, OUTPUT);
 }
 
+void loop()
+{
+  // here is where you'd put code that needs to be running all the time.
+
+  // check to see if it's time to blink the LED; that is, if the
+  // difference between the current time and last time you blinked
+  // the LED is bigger than the interval at which you want to
+  // blink the LED.
+  unsigned long currentMillis = millis();
+ 
+  if(currentMillis - previousMillis >= interval) {
+    // save the last time you blinked the LED 
+    previousMillis = currentMillis;   
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState == LOW)
+      ledState = HIGH;
+    else
+      ledState = LOW;
+
+    // set the LED with the ledState of the variable:
+    digitalWrite(ledPin, ledState);
+  }
+}
